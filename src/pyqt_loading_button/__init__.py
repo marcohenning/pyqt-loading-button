29e70f68d2786472e016cff4d8244ce60f6e0f3c
Marco Henning <marcohnngpersonal@gmail.com>
2024-09-10 17:28:11 +0200

    Set up project structure

---
+++ src/pyqt_loading_button/__init__.py
@@ -0,0 +1,1 @@
+from .loading_button import LoadingButton
---
+++ src/pyqt_loading_button/loading_button.py
@@ -0,0 +1,7 @@
+from qtpy.QtWidgets import QPushButton
+
+
+class LoadingButton(QPushButton):
+
+    def __init__(self, parent=None):
+        super(LoadingButton, self).__init__(parent)
